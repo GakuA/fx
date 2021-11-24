1868dbe62a7b1a91e50c67602ecad1f8cebe185c
GakuA <akazawagaku@gmail.com>
2021-11-24 14:21:50 +0900

    first

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+	<head>
+		<meta charset="utf-8">
+		<title>FX計算!</title>
+	</head>
+	<body>
+		<div id="usdjpy">
+			<div class="title">USD/JPY</div>
+			<div class="price">100.11</div>
+		</div>
+		<div id="eurjpy">
+			<div class="title">EUR/JPY</div>
+			<div class="price">100.11</div>
+		</div>
+		<div id="audjpy">
+			<div class="title">AUD/JPY</div>
+			<div class="price">100.11</div>
+		</div>
+		<div id="gbpjpy">
+			<div class="title">GBP/JPY</div>
+			<div class="price">100.11</div>
+		</div>
+	</body>
+</html>
