c725b3d8b552116fb740a6dd16cdc2416298c1e2
GakuA <akazawagaku@gmail.com>
2021-11-25 20:40:33 +0900

    fix

--- getPrice.php
+++ getPrice.php
@@ -1,9 +1,17 @@
 <?php
 
-	echo price();
+	$count = 1;
+	while ($count <= 3) {
+		$count++;
+		$price = price();
+		if ($price) {
+			break;
+		}
+	}
+	echo $price;
 
 function price() {
-	if(preg_match("/<span id=\"USDJPY_chart_bid\"[^>]*>(.*?)<\/span>/i", "https://info.finance.yahoo.co.jp/fx/list/", $matches)){
+	if(preg_match("/<span id=\"USDJPY_chart_bid\"[^>]*>(.*?)\<\/span\>/i", "https://info.finance.yahoo.co.jp/fx/list/", $matches)){
 		return $matches[1];
 	} else {
 		return false;
