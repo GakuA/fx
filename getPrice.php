ad63fc00e15eb88c09b9d34b7c890d04be10e1ce
GakuA <akazawagaku@gmail.com>
2021-11-25 21:36:21 +0900

    fix

--- getPrice.php
+++ getPrice.php
@@ -1,34 +1,30 @@
 <?php
 
-	$prices = price();
+	$html = @file_get_contents("https://info.finance.yahoo.co.jp/fx/list/");
+	if (preg_match("/<span id\=\"USDJPY_chart_bid\"[^>]*>(.*?)<\/span>/i", $html, $matches)) {
+		$usd = $matches[1];
+	} else {
+		$usd = "";
+	}
+
+	if (preg_match("/<span id\=\"EURJPY_chart_bid\"[^>]*>(.*?)<\/span>/i", $html, $matches)) {
+		$eur = $matches[1];
+	} else {
+		$eur = "";
+	}
+
+	if (preg_match("/<span id\=\"AUDJPY_chart_bid\"[^>]*>(.*?)<\/span>/i", $html, $matches)) {
+		$aud = $matches[1];
+	} else {
+		$aud = "";
+	}
+
+	if (preg_match("/<span id\=\"GBPJPY_chart_bid\"[^>]*>(.*?)<\/span>/i", $html, $matches)) {
+		$gbp = $matches[1];
+	} else {
+		$gbp = "";
+	}
+
+	$prices = array("usd" => $usd, "eur" => $eur, "aud" => $aud, "gbp" => $gbp);
 
 	echo json_encode($prices);
-
-	function price() {
-		$html = @file_get_contents("https://info.finance.yahoo.co.jp/fx/list/");
-		if (preg_match("/<span id\=\"USDJPY_chart_bid\"[^>]*>(.*?)<\/span>/i", $html, $matches)) {
-			$usd = $matches[1];
-		} else {
-			$usd = "";
-		}
-
-		if (preg_match("/<span id\=\"EURJPY_chart_bid\"[^>]*>(.*?)<\/span>/i", $html, $matches)) {
-			$eur = $matches[1];
-		} else {
-			$eur = "";
-		}
-
-		if (preg_match("/<span id\=\"AUDJPY_chart_bid\"[^>]*>(.*?)<\/span>/i", $html, $matches)) {
-			$aud = $matches[1];
-		} else {
-			$aud = "";
-		}
-
-		if (preg_match("/<span id\=\"GBPJPY_chart_bid\"[^>]*>(.*?)<\/span>/i", $html, $matches)) {
-			$gbp = $matches[1];
-		} else {
-			$gbp = "";
-		}
-
-		return array("usd" => $usd, "eur" => $eur, "aud" => $aud, "gbp" => $gbp);
-	}
