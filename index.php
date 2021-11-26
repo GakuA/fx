7253ea04c728177314c98dcbe8db012253137f22
GakuA <akazawagaku@gmail.com>
2021-11-26 12:16:56 +0900

    textbox

--- index.php
+++ index.php
@@ -7,21 +7,24 @@
 		<script src="js/load.js"></script>
 	</head>
 	<body>
+		<div>
+			<input type="text">
+		</div>
 		<div id="usdjpy">
 			<div class="title">USD/JPY</div>
-			<div class="price">111.11</div>
+			<div class="price"></div>
 		</div>
 		<div id="eurjpy">
 			<div class="title">EUR/JPY</div>
-			<div class="price">100.11</div>
+			<div class="price"></div>
 		</div>
 		<div id="audjpy">
 			<div class="title">AUD/JPY</div>
-			<div class="price">100.11</div>
+			<div class="price"></div>
 		</div>
 		<div id="gbpjpy">
 			<div class="title">GBP/JPY</div>
-			<div class="price">100.11</div>
+			<div class="price"></div>
 		</div>
 	</body>
 </html>
